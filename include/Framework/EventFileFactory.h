6aff479061bb6ebd2b45f0746b406e4cab0c2d0a
Omar Moreno <email@omarmoreno.net>
2020-10-15 23:24:48 -0700

    Update to use smart pointers.  Intead of parameters, just pass the full set of parameters to the create function.

--- include/Framework/EventFileFactory.h
+++ include/Framework/EventFileFactory.h
@@ -9,11 +9,22 @@
 /*~~~~~~~~~~~~~~~*/
 /*   Framework   */
 /*~~~~~~~~~~~~~~~*/
-#include "Framework/Configure/Parameters.h"
 #include "Framework/EventFile.h"
 
 namespace framework {
 
+/**
+ * Factory that creates an EventFile object of the given type.
+ *
+ * The following EventFile types are supported:
+ *  type | class
+ *  ------------
+ *  root | RootEventFile
+ *  lcio | LcioEventFile
+ *
+ * The type is set through the python config file and passed to the constructor
+ * of this class via a parameter.
+ */
 class EventFileFactory {
 
 public:
@@ -26,10 +37,23 @@ public:
   /**
    * Create an instance of the EventFile of the given type.
    *
-   * @parser[in] type String type of the EventFile that needs to be created.
+   * @param[in] type String type of the EventFile that needs to be created.
+   * @param[in] filename The name of the file that will be read in or created.
+   * @param[in] parent A pointer to the EventFile that will be cloned.
+   * @param[in] is_output_file True if this is an output file, false otherwise.
+   * @param[in] is_single_output True if only a single output file is being
+   *  written.
+   * @param[in] compression_level The compression level of the file.
+   *
+   * @return[out] A smart pointer to an EventFile of the given type.
+   *
+   * @throw FileTypeDoesNotExists Thrown if the given event file type has not 
+   *  been registered. 
    */
-  EventFile *createEventFile(const std::string &name,
-                             ldmx::Parameters &parameters);
+  std::unique_ptr<EventFile>
+  createEventFile(const std::string &type, const std::string &filename,
+                  EventFile *parent, bool is_output_file, bool is_single_output,
+                  int compression_level);
 
 private:
   /// Static instance of this class
@@ -50,7 +74,7 @@ private:
   void registerEventFile(const std::string &name, createFunc create);
 
   // Mapping between an EventFile type and its create function
-  std::map<std::string, createFunc> parser_map;
+  std::map<std::string, createFunc> event_file_map;
 
 }; // EventFileFactory
 } // namespace framework
--- src/Framework/EventFileFactory.cxx
+++ src/Framework/EventFileFactory.cxx
@@ -1,27 +1,52 @@
 
 #include "Framework/EventFileFactory.h"
 
+/*~~~~~~~~~~~~~~~*/
+/*   Framework   */
+/*~~~~~~~~~~~~~~~*/
+#include "Framework/Exception/Exception.h" 
+
+/*~~~~~~~~~~~~~*/
+/*   SimCore   */
+/*~~~~~~~~~~~~~*/
+#include "Framework/LcioEventFile.h"
+
 namespace framework {
+
 EventFileFactory *EventFileFactory::instance_ = nullptr;
 
 EventFileFactory *EventFileFactory::getInstance() {
+  // If an EventFileFactory hasn't been instantiated, create and return it.
+  // Otherwise, just return the existing one.
   if (!instance_)
     instance_ = new EventFileFactory;
   return instance_;
 }
 
-EventFileFactory::EventFileFactory() {}
+EventFileFactory::EventFileFactory() {
+  // Register the event file types
+  // TODO: At some point the registration should be dictated by the python
+  // config.
+  registerEventFile("lcio", &LcioEventFile::create);
+}
 
 void EventFileFactory::registerEventFile(const std::string &name,
                                          createFunc create) {
-  parser_map[name] = create;
+  event_file_map[name] = create;
 }
 
-EventFile *EventFileFactory::createEventFile(const std::string &name,
-                                             ldmx::Parameters &parameters) {
-  auto it{parser_map.find(name)};
-  if (it != parser_map.end())
-    return it->second(parameters);
-  return nullptr;
+std::unique_ptr<EventFile> EventFileFactory::createEventFile(
+    const std::string &type, const std::string &filename, EventFile *parent,
+    bool is_output_file, bool is_single_output, int compression_level) {
+
+  // Check if the event file type exists.  If so, create the EventFile.
+  // Otherwise, just throw an exception.
+  auto it{event_file_map.find(type)};
+  if (it != event_file_map.end())
+    return it->second(filename, parent, is_output_file, is_single_output,
+                      compression_level);
+  EXCEPTION_RAISE("FileTypeDoesNotExists",
+                  "The provided file type does not exists.");
 }
+
 } // namespace framework
